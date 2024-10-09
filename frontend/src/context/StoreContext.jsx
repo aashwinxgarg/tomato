1133cb2bef7c6cfa1fc267d21335e68617d1941c
Aashwin Garg <aashwinxgarg@gmail.com>
2024-10-09 16:49:13 +0530

    create user order page

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -43,7 +43,6 @@ const StoreContextProvider = (props)=>{
     const fetchFoodList = async () => {
         const response = await axios.get(url+"/api/food/list")
         setFoodList(response.data.data)
-        console.log(response.data.data);
         
     }
 
